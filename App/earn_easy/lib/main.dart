5a514e3bcb8233a584e19732a885b161a060b94a
PritomKumar <bsse0919@iit.du.ac.bd>
2020-07-31 20:13:44 +0600

    Added google Maps Api

--- App/earn_easy/lib/main.dart
+++ App/earn_easy/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:earneasy/app_screens/search.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Search(),
     );
   }
 }
