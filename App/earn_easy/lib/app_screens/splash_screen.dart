4a031685abde63c533a1cc0cf8dd3cd258738de3
PritomKumar <bsse0919@iit.du.ac.bd>
2020-08-02 00:26:09 +0600

    Splash Screen Implement

--- App/earn_easy/lib/app_screens/splash_screen.dart
+++ App/earn_easy/lib/app_screens/splash_screen.dart
@@ -1,16 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _SplashScreenState();
   }
-
 }
 
-class _SplashScreenState extends State<SplashScreen>{
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => debugPrint("Timer Done TODO"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,10 +36,58 @@ class _SplashScreenState extends State<SplashScreen>{
               color: theme.primaryColorLight,
             ),
           ),
-
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.monetization_on,
+                          color: Colors.greenAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "earnEasy",
+                        textDirection: TextDirection.ltr,
+                        style: theme.textTheme.headline4,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.redAccent,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "A Mobile Crowd Sourcing Platform",
+                      textDirection: TextDirection.ltr,
+                      style: theme.textTheme.subtitle1,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
-
 }
