46cc6df05fb80c7c7164b65c1ac014590b4e0790
PritomKumar <bsse0919@iit.du.ac.bd>
2020-08-19 22:40:17 +0600

    Navigation user account face added

--- App/earn_easy/lib/app_screens/home/side_drawer.dart
+++ App/earn_easy/lib/app_screens/home/side_drawer.dart
@@ -10,38 +10,77 @@ class SideDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Drawer(
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text("Home"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Gigs()));
-              },
-            ),
-            ListTile(
-              title: Text("My Profile"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Profile()));
-              },
-            ),
-            ListTile(
-              title: Text("Notifications"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => NotificationPage()));
-              },
-            ),
-            ListTile(
-              title: Text("Settings"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Setting()));
-              },
-            ),
-          ],
+      child: SafeArea(
+        child: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountEmail: Text("example@gmail.com"),
+                accountName: Text("Name"),
+                currentAccountPicture: CircleAvatar(
+                  child: Text(
+                    "P",
+                    style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  backgroundColor: Colors.blue[600],
+                ),
+                otherAccountsPictures: <Widget>[
+                  CircleAvatar(
+                    child: Text(
+                      "P",
+                    ),
+                    backgroundColor: Colors.white,
+                  ),
+                ],
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Gigs()));
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.person_outline),
+                title: Text("My Profile"),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Profile()));
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.notifications_active),
+                title: Text("Notifications"),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => NotificationPage()));
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Setting()));
+                },
+              ),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
