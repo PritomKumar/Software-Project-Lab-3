ca2b63beea121aa47b06ea94151987d9ecc9ebb8
PritomKumar <bsse0919@iit.du.ac.bd>
2020-08-30 15:56:26 +0600

    Debug [core/no-app] No Firebase App '[DEFAULT]' has been created - call Firebase.initializeApp() bug

--- App/earn_easy/lib/main.dart
+++ App/earn_easy/lib/main.dart
@@ -1,20 +1,11 @@
-import 'package:earneasy/app_screens/wrapper.dart';
-import 'package:earneasy/models/user.dart';
-import 'package:earneasy/services/auth.dart';
 
-import 'app_screens/home/login_screen.dart';
-import 'package:earneasy/app_screens/search.dart';
-import 'package:earneasy/app_screens/search_screen_loader.dart';
 import 'package:earneasy/app_screens/splash_screen.dart';
-import 'package:earneasy/services/geolocator_service.dart';
-import 'package:earneasy/services/places_service.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
-import 'models/place.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -22,16 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Parking App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: SplashScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Parking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: SplashScreen(),
     );
   }
 }
