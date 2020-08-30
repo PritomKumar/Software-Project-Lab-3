6760e84c7d32b971b6eb46eef94c531078eeb1d3
PritomKumar <bsse0919@iit.du.ac.bd>
2020-08-31 01:58:14 +0600

    Set up useraccount data stream from uid

--- App/earn_easy/lib/services/firestore_databse.dart
+++ App/earn_easy/lib/services/firestore_databse.dart
@@ -3,62 +3,63 @@ import 'package:earneasy/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 
-class DatabaseService{
+class DatabaseService {
+  final CollectionReference userProfiles =
+      FirebaseFirestore.instance.collection("Users");
+  final String uid = FirebaseAuth.instance.currentUser.uid;
 
-  final CollectionReference userProfiles = FirebaseFirestore.instance.collection("Users");
-
-  bool isLoggedIn(){
-    if(FirebaseAuth.instance.currentUser!=null){
+  bool isLoggedIn() {
+    if (FirebaseAuth.instance.currentUser != null) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
 
-  Future updateUserData(UserAccount userAccount){
-    if(isLoggedIn()){
+  Future updateUserData(UserAccount userAccount) {
+    if (isLoggedIn()) {
       userProfiles.doc(userAccount.uid).set({
-        "name" : userAccount.name ?? "",
-        "email" : userAccount.email ?? "",
-        "photoUrl" : userAccount.photoUrl ?? "",
-        "phoneNumber" : userAccount.phoneNumber ?? "",
-        "birthDay" : userAccount.birthDay ?? DateTime(1000,1,1),
-        "gender" : userAccount.gender ?? "",
-        "streetAddress" : userAccount.streetAddress ?? "",
-        "city" : userAccount.city ?? "",
-        "state" : userAccount.state ?? "",
-        "zipCode" : userAccount.zipCode ?? "",
-        "bio" : userAccount.bio ?? "",
-        "occupation" : userAccount.occupation ?? "",
-        "maritalStatus" : userAccount.maritalStatus ?? "",
-        "educationLevel" : userAccount.educationLevel ?? "",
-        "employmentStatus" : userAccount.employmentStatus ?? "",
-        "householdIncome" : userAccount.householdIncome ?? "",
-        "level" : userAccount.level ?? 1,
-        "type" : userAccount.type ?? "worker",
-        "writeAccess" : userAccount.writeAccess ?? false,
+        "name": userAccount.name ?? "",
+        "email": userAccount.email ?? "",
+        "photoUrl": userAccount.photoUrl ?? "",
+        "phoneNumber": userAccount.phoneNumber ?? "",
+        "birthDay": userAccount.birthDay ?? DateTime(1000, 1, 1),
+        "gender": userAccount.gender ?? "",
+        "streetAddress": userAccount.streetAddress ?? "",
+        "city": userAccount.city ?? "",
+        "state": userAccount.state ?? "",
+        "zipCode": userAccount.zipCode ?? "",
+        "bio": userAccount.bio ?? "",
+        "occupation": userAccount.occupation ?? "",
+        "maritalStatus": userAccount.maritalStatus ?? "",
+        "educationLevel": userAccount.educationLevel ?? "",
+        "employmentStatus": userAccount.employmentStatus ?? "",
+        "householdIncome": userAccount.householdIncome ?? "",
+        "level": userAccount.level ?? 1,
+        "type": userAccount.type ?? "worker",
+        "writeAccess": userAccount.writeAccess ?? false,
       });
-    }
-    else{
+    } else {
       return null;
     }
   }
 
   Future userAccountOfCurrentUser() async {
-    var result = await userProfiles.doc(FirebaseAuth.instance.currentUser.uid).get();
+    var result =
+        await userProfiles.doc(FirebaseAuth.instance.currentUser.uid).get();
     print(result.data());
   }
 
-//  Future<UserAccount> userAccountOfCurrentUser() async {
-//    var result = await userProfiles.doc(FirebaseAuth.instance.currentUser.uid).get();
-//    print(result.data());
-//    return UserAccount(
-//      uid: result.id,
-//      name: result.data()["name"],
-//      email: result.data()["email"],
-//      photoUrl: result.data()["photoUrl"],
-//    );
-//  }
+  UserAccount _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserAccount(
+      uid: uid,
+      name: snapshot.data()['name'] ?? "",
+      email: snapshot.data()['email'] ?? "",
+      photoUrl: snapshot.data()['photoUrl'] ?? "",
+    );
+  }
 
+  Stream<UserAccount> get userData {
+    return userProfiles.doc(uid).snapshots().map(_userDataFromSnapshot);
+  }
 }
