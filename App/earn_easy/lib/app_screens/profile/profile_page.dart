50091cbd0b6431bccb4a92d42bbfc739305d98df
PritomKumar <bsse0919@iit.du.ac.bd>
2020-08-31 14:43:45 +0600

    Name in userAccount change into firstName and lastName

--- App/earn_easy/lib/app_screens/profile/profile_page.dart
+++ App/earn_easy/lib/app_screens/profile/profile_page.dart
@@ -169,6 +169,152 @@ class Profile extends StatefulWidget {
 //  }
 //}
 
+//class _ProfileState extends State<Profile> {
+//  final _formKey = GlobalKey<FormState>();
+//  var emailController = TextEditingController();
+//  var passwordController = TextEditingController();
+//  var firstNameController = TextEditingController();
+//  var lastNameController = TextEditingController();
+//  bool isloading = false;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    var size = MediaQuery.of(context).size;
+//    var user = Provider.of<UserAccount>(context);
+//    setState(() {
+//      if (user != null) {
+//        isloading = true;
+//      }
+//    });
+//
+//    if (isloading) {
+//      firstNameController.text = user.firstName;
+//      lastNameController.text = user.lastName;
+//      emailController.text = user.email;
+//
+//      return MaterialApp(
+//        debugShowCheckedModeBanner: false,
+//        home: Scaffold(
+//          drawer: SideDrawer(),
+//          appBar: AppBar(
+//            title: Text('Profile'),
+//          ),
+//          body: Form(
+//            key: _formKey,
+//            child: Center(
+//              child: Container(
+//                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+//                child: ListView(
+//                  children: <Widget>[
+//                    Row(
+//                      children: <Widget>[
+//                        Text(
+//                          "First Name :",
+//                          style: TextStyle(fontSize: size.width / 25),
+//                        ),
+//                        SizedBox(
+//                          width: size.width / 40,
+//                        ),
+//                        Expanded(
+//                          child: TextFormField(
+//                            controller: firstNameController,
+//                            decoration: InputDecoration(hintText: "First Name"),
+//                            validator: (value) {
+//                              return value.isEmpty ? "Enter First Name" : null;
+//                            },
+//                          ),
+//                        ),
+//                      ],
+//                    ),
+//                    Row(
+//                      children: <Widget>[
+//                        Text(
+//                          "Last Name :",
+//                          style: TextStyle(fontSize: size.width / 25),
+//                        ),
+//                        SizedBox(
+//                          width: size.width / 40,
+//                        ),
+//                        Expanded(
+//                          child: TextFormField(
+//                            controller: lastNameController,
+//                            decoration: InputDecoration(hintText: "Last Name"),
+//                            validator: (value) {
+//                              return value.isEmpty ? "Enter Last Name" : null;
+//                            },
+//                          ),
+//                        ),
+//                      ],
+//                    ),
+//                    Row(
+//                      children: <Widget>[
+//                        Text(
+//                          "Email :",
+//                          style: TextStyle(fontSize: size.width / 25),
+//                        ),
+//                        SizedBox(
+//                          width: size.width / 40,
+//                        ),
+//                        Expanded(
+//                          child: TextFormField(
+//                            controller: emailController,
+//                            decoration: InputDecoration(hintText: "Email"),
+//                            validator: (value) {
+//                              return value.isEmpty ? "Enter Email" : null;
+//                            },
+//                          ),
+//                        ),
+//                      ],
+//                    ),
+//                    RaisedButton(
+//                      color: Colors.pink[400],
+//                      child: Text(
+//                        "Update",
+//                        style: TextStyle(color: Colors.white),
+//                      ),
+//                      onPressed: () async {
+//                        // TODO : Complete UserAccount
+//
+//                        if (_formKey.currentState.validate()) {
+//                          await DatabaseService().updateUserData(UserAccount(
+//                            firstName:
+//                                firstNameController.text ?? user.firstName,
+//                            lastName: lastNameController.text ?? user.lastName,
+//                            email: emailController.text ?? user.email,
+//                            photoUrl: user.photoUrl,
+//                            phoneNumber: user.phoneNumber,
+//                            birthDay: user.birthDay,
+//                            gender: user.gender,
+//                            streetAddress: user.streetAddress,
+//                            city: user.city,
+//                            state: user.state,
+//                            zipCode: user.zipCode,
+//                            bio: user.bio,
+//                            occupation: user.occupation,
+//                            maritalStatus: user.maritalStatus,
+//                            educationLevel: user.educationLevel,
+//                            employmentStatus: user.employmentStatus,
+//                            householdIncome: user.householdIncome,
+//                            level: user.level,
+//                            type: user.type,
+//                            writeAccess: user.writeAccess,
+//                          ));
+//                        }
+//                      },
+//                    ),
+//                  ],
+//                ),
+//              ),
+//            ),
+//          ),
+//        ),
+//      );
+//    } else {
+//      return Loading();
+//    }
+//  }
+//}
+
 class _ProfileState extends State<Profile> {
   final _formKey = GlobalKey<FormState>();
   var emailController = TextEditingController();
@@ -278,7 +424,7 @@ class _ProfileState extends State<Profile> {
                         if (_formKey.currentState.validate()) {
                           await DatabaseService().updateUserData(UserAccount(
                             firstName:
-                                firstNameController.text ?? user.firstName,
+                            firstNameController.text ?? user.firstName,
                             lastName: lastNameController.text ?? user.lastName,
                             email: emailController.text ?? user.email,
                             photoUrl: user.photoUrl,
