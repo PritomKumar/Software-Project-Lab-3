b95b6fffad9e3c5a15c89ac2eaffc0ac2e9e5597
PritomKumar <bsse0919@iit.du.ac.bd>
2020-09-11 15:27:55 +0600

    FineTune Start and end time to gig show page

--- App/earn_easy/lib/app_screens/gigs/gig_page.dart
+++ App/earn_easy/lib/app_screens/gigs/gig_page.dart
@@ -11,6 +11,7 @@ class GigPage extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> lol = List<String>();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: gig.title,
       home: Scaffold(
         appBar: AppBar(
@@ -22,61 +23,99 @@ class GigPage extends StatelessWidget {
               alignment: Alignment.center,
               child: ListView(
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(FontAwesomeIcons.calendarCheck,color: Colors.black,),
-                      SizedBox(width: 2.0,),
-                      Text(
-                        "Starts: ",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                  Container(
+                    color: Colors.grey[300],
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.calendarCheck,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              "Starts: ",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              gig.startTime
+                                      .toDate()
+                                      .day
+                                      .toString()
+                                      .padLeft(2, '0') +
+                                  "/" +
+                                  gig.startTime
+                                      .toDate()
+                                      .month
+                                      .toString()
+                                      .padLeft(2, '0') +
+                                  "/" +
+                                  gig.startTime.toDate().year.toString(),
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      SizedBox(width: 2.0,),
-                      Text(
-                        gig.startTime.toDate().day
-                            .toString()
-                            .padLeft(2, '0') +
-                            "/" +
-                            gig.startTime.toDate().month
-                                .toString()
-                                .padLeft(2, '0') +
-                            "/" +
-                            gig.startTime.toDate().year.toString(),
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
+                        SizedBox(
+                          width: 10.0,
                         ),
-                      ),
-                      SizedBox(width: 5.0,),
-                      Icon(FontAwesomeIcons.calendarTimes,color: Colors.black,),
-                      SizedBox(width: 2.0,),
-                      Text(
-                        "Ends: ",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.calendarTimes,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              "Ends: ",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              gig.endTime
+                                      .toDate()
+                                      .day
+                                      .toString()
+                                      .padLeft(2, '0') +
+                                  "/" +
+                                  gig.endTime
+                                      .toDate()
+                                      .month
+                                      .toString()
+                                      .padLeft(2, '0') +
+                                  "/" +
+                                  gig.endTime.toDate().year.toString(),
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      SizedBox(width: 2.0,),
-                      Text(
-                        gig.endTime.toDate().day
-                            .toString()
-                            .padLeft(2, '0') +
-                            "/" +
-                            gig.endTime.toDate().month
-                                .toString()
-                                .padLeft(2, '0') +
-                            "/" +
-                            gig.endTime.toDate().year.toString(),
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   Text(gig.money.toString()),
                   Text(gig.description),
