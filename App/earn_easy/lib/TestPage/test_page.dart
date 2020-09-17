ba202c584b91f0e197e7ad7fdf957f7b0dcd6438
PritomKumar <bsse0919@iit.du.ac.bd>
2020-09-17 23:11:49 +0600

    Problem fixed for not changing value

--- App/earn_easy/lib/TestPage/test_page.dart
+++ App/earn_easy/lib/TestPage/test_page.dart
@@ -8,6 +8,9 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
+  bool isExpanded = false;
+  static List<String> sortOptionsArray = ["Distance", "Money", "Title"];
+  String sortOption = sortOptionsArray[0];
   @override
   Widget build(BuildContext context) {
     List<GigMini> gigs = List<GigMini>();
@@ -31,9 +34,8 @@ class _TestState extends State<Test> {
       money: 400,
       gigId: "fsdfuhfsjkdfhu",
     ));
-    bool isExpanded = false;
-    List<String> sortOptionsArray = ["Distance", "Money", "Title"];
-    String sortOption = sortOptionsArray[0];
+
+
     //sort function
     gigs.sort((a, b) => a.money.compareTo(b.money));
     return MaterialApp(
@@ -49,39 +51,31 @@ class _TestState extends State<Test> {
             shadowColor: Colors.green,
             color: Colors.blue,
             child: ExpansionTile(
-              onExpansionChanged: (value) {
+              onExpansionChanged: (bool value) {
                 setState(() {
                   isExpanded = value;
-                  print(isExpanded);
                 });
               },
               title: Text("Title"),
               backgroundColor: Colors.white,
-              subtitle: Text("Subtitle"),
-              trailing: SizedBox(
-                width: 100.0,
-                child: Expanded(
-                  child: PopupMenuButton(
-                    initialValue: "Sort By",
-                    tooltip: "Sort Items",
-                    elevation: 5.0,
-                    enabled: isExpanded,
-                    icon: Icon(Icons.sort,size: 30.0,),
-                    itemBuilder: (context) {
-                      return sortOptionsArray.map((String choice) {
-                        return PopupMenuItem(
-                          value: choice,
-                          child: Text(choice),
-                        );
-                      }).toList();
-                    },
-                    onSelected: (value) {
-                      setState(() {
-                        sortOption = value;
-                      });
-                    },
-                  ),
-                ),
+              subtitle: Text("Subtitle "),
+              trailing: PopupMenuButton(
+                elevation: 5.0,
+                enabled: isExpanded,
+                icon: Text(sortOption),
+                itemBuilder: (context) {
+                  return sortOptionsArray.map((String choice) {
+                    return PopupMenuItem(
+                      value: choice,
+                      child: Text(choice),
+                    );
+                  }).toList();
+                },
+                onSelected: (value) {
+                  setState(() {
+                    sortOption = value;
+                  });
+                },
               ),
               children: <Widget>[
                 SizedBox(
