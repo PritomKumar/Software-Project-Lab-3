75e98e6540e660591424fe97d0e5c735f3944878
PritomKumar <bsse0919@iit.du.ac.bd>
2020-09-17 16:46:16 +0600

    Upgrade for TextStyle

--- App/earn_easy/lib/TestPage/test_page.dart
+++ App/earn_easy/lib/TestPage/test_page.dart
@@ -1,3 +1,4 @@
+import 'package:earneasy/models/gig.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,6 +10,27 @@ class Test extends StatefulWidget {
 class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
+    List<GigMini> gigs = List<GigMini>();
+    gigs.add(GigMini(
+      title: "First",
+      money: 100,
+      gigId: "fsdfuhfsjkdfhu",
+    ));
+    gigs.add(GigMini(
+      title: "Second",
+      money: 200,
+      gigId: "fsdfuhfsjkdfhu",
+    ));
+    gigs.add(GigMini(
+      title: "Third",
+      money: 300,
+      gigId: "fsdfuhfsjkdfhu",
+    ));
+    gigs.add(GigMini(
+      title: "Fourth",
+      money: 400,
+      gigId: "fsdfuhfsjkdfhu",
+    ));
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
@@ -21,29 +43,40 @@ class _TestState extends State<Test> {
             elevation: 15.0,
             shadowColor: Colors.green,
             color: Colors.blue,
-            child: SizedBox(
-              //height: MediaQuery.of(context).size.height / 2,
-              child: ExpansionTile(
-                title: Text("Title"),
-                backgroundColor: Colors.redAccent,
-                subtitle: Text("Subtitle"),
-                trailing: Icon(FontAwesomeIcons.arrowDown),
-                children: <Widget>[
-                  ListTile(
-                    title: Text("Item 1"),
-                    trailing: Icon(Icons.hourglass_empty),
+            child: ExpansionTile(
+              title: Text("Title"),
+              backgroundColor: Colors.white,
+              subtitle: Text("Subtitle"),
+              trailing: Icon(FontAwesomeIcons.arrowDown),
+              children: <Widget>[
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: ListView.builder(
+                    itemCount: gigs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(gigs[index].title),
+                        subtitle: Text("TODO"),
+                        trailing: Text(
+                          gigs[index].money.toString(),
+                          textHeightBehavior: TextHeightBehavior(
+                            applyHeightToFirstAscent: true,
+                            applyHeightToLastDescent: true,
+                          ),
+                          textScaleFactor: 1.5,
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onTap: () {
+                          print(gigs[index].toMap());
+                        },
+                      );
+                    },
                   ),
-                  ListTile(
-                    title: Text("Item 2"),
-                    trailing: Icon(Icons.brush),
-                  ),
-                  ListTile(
-                    title: Text("Item 3"),
-                    trailing: Icon(Icons.settings_input_composite),
-                  ),
-
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
