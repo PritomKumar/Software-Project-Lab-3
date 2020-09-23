437d8b390005085681e7338b705b061ffb7dd678
PritomKumar <bsse0919@iit.du.ac.bd>
2020-09-24 02:43:42 +0600

    Create uploader Functionalities

--- App/earn_easy/lib/TestPage/test_page.dart
+++ App/earn_easy/lib/TestPage/test_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:earneasy/models/gig.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
@@ -241,7 +242,7 @@ class _ImageCaptureState extends State<ImageCapture> {
       ),
       body: ListView(
         children: <Widget>[
-          if(_imageFile!=null) ...[
+          if (_imageFile != null) ...[
             Image.file(_imageFile),
             Row(
               children: <Widget>[
@@ -255,7 +256,6 @@ class _ImageCaptureState extends State<ImageCapture> {
                 ),
               ],
             ),
-
             Uploader(file: _imageFile),
           ]
         ],
@@ -268,14 +268,66 @@ class Uploader extends StatefulWidget {
   final File file;
 
   const Uploader({Key key, this.file}) : super(key: key);
+
   @override
   _UploaderState createState() => _UploaderState();
 }
 
 class _UploaderState extends State<Uploader> {
+  final FirebaseStorage _firebaseStorage =
+      FirebaseStorage(storageBucket: "gs://earneasy-5e92c.appspot.com");
+
+  StorageUploadTask _uploadTask;
+
+  void _startUpload() {
+    String filePath =
+        "images/${DateTime.now().millisecondsSinceEpoch.toString()}.png";
+    setState(() {
+      _uploadTask = _firebaseStorage.ref().child(filePath).putFile(widget.file);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (_uploadTask != null) {
+      return StreamBuilder<StorageTaskEvent>(
+        stream: _uploadTask.events,
+        builder: (context, snapshot) {
+          var event = snapshot?.data?.snapshot;
+          double progressPercent =
+              event != null ? event.bytesTransferred / event.totalByteCount : 0;
+
+          return ListView(
+            children: <Widget>[
+              if (_uploadTask.isComplete) Text("Upload Done"),
+              if (_uploadTask.isPaused)
+                FlatButton(
+                  child: Icon(Icons.play_arrow),
+                  onPressed: _uploadTask.resume,
+                ),
+              if (_uploadTask.isInProgress)
+                FlatButton(
+                  child: Icon(Icons.pause),
+                  onPressed: _uploadTask.pause,
+                ),
+
+              LinearProgressIndicator(
+                value: progressPercent,
+              ),
+
+              Text(
+                "${(progressPercent*100).toStringAsFixed(2)} % "
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      return FlatButton.icon(
+        label: Text("Upload To FireBase"),
+        icon: Icon(Icons.cloud_upload),
+        onPressed: _startUpload,
+      );
+    }
   }
 }
-
