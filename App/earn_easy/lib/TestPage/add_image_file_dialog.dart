dd6011ba55365b97d2d03208103ee18a55efb05c
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-01 23:44:17 +0600

    Add solution to various problems in add_image_file_dialog.dart

--- App/earn_easy/lib/TestPage/add_image_file_dialog.dart
+++ App/earn_easy/lib/TestPage/add_image_file_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddImageTaskDialog extends StatefulWidget {
@@ -7,10 +8,12 @@ class AddImageTaskDialog extends StatefulWidget {
 
 class _AddImageTaskDialogState extends State<AddImageTaskDialog> {
   var taskDescriptionController = TextEditingController();
+  var numberofImageController = TextEditingController();
   double numberOfTaskImage = 1.0;
 
   @override
   Widget build(BuildContext context) {
+    numberofImageController.text = numberOfTaskImage.round().toString();
     return SimpleDialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       //this right here
@@ -33,12 +36,67 @@ class _AddImageTaskDialogState extends State<AddImageTaskDialog> {
         Padding(
           padding: EdgeInsets.all(15.0),
           child: Wrap(
-            alignment: WrapAlignment.center,
+            alignment: WrapAlignment.spaceEvenly,
+            runAlignment: WrapAlignment.start,
             children: <Widget>[
+              // SizedBox(
+              //   width: 30.0,
+              //   child: TextFormField(
+              //     controller: numberofImageController,
+              //     decoration: InputDecoration(hintText: "Image number"),
+              //     autovalidate: true,
+              //     textAlign: TextAlign.center,
+              //     keyboardType: TextInputType.number,
+              //     maxLines: null,
+              //     onFieldSubmitted: (value) {
+              //       setState(() {
+              //         numberOfTaskImage =
+              //             double.parse(numberofImageController.text);
+              //       });
+              //     },
+              //     validator: (value) {
+              //       return value.isEmpty ? "Enter number of image" : null;
+              //     },
+              //   ),
+              // ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      child: Text(
+                        "Minimum number of image : ",
+                        textScaleFactor: 1.05,
+                        maxLines: null,
+                        //overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "${numberOfTaskImage.round()}",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 2.0,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
               Slider(
                 value: numberOfTaskImage,
                 min: 1.0,
-                max: 100.0,
+                max: 50.0,
                 onChanged: (value) {
                   setState(() {
                     numberOfTaskImage = value;
