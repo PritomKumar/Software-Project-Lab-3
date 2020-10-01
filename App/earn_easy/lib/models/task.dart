fa17d310856e7e13642039a9adca2b19e84738e3
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-01 19:49:41 +0600

    Task header is removed after deliberation

--- App/earn_easy/lib/models/task.dart
+++ App/earn_easy/lib/models/task.dart
@@ -3,15 +3,15 @@ import 'package:flutter/cupertino.dart';
 class ImageTask {
   final String type;
   final String submittedUser;
-  final String taskHeader;
   final String taskDescription;
+  final int numberOfImages;
   final List<String> submittedImageUrlList;
 
   ImageTask({
     @required this.type,
-    @required this.submittedUser,
-    @required this.taskHeader,
+    this.submittedUser,
     @required this.taskDescription,
+    @required this.numberOfImages,
     @required this.submittedImageUrlList,
   });
 }
