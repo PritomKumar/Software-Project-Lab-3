a9dd12d506e451219c50beffed69e5723ca92f1f
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-03 11:41:39 +0600

    Breaking bug

--- App/earn_easy/lib/TestPage/add_gig_test.dart
+++ App/earn_easy/lib/TestPage/add_gig_test.dart
@@ -372,6 +372,7 @@ class _GigAddState extends State<GigAddTest> {
                                       onPressed: () {
                                         setState(() {
                                           //TODO Task Delete
+                                          taskList.removeAt(index);
                                         });
                                       },
                                     ),
