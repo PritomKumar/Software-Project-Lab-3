36a68a8cea31e24e6f1b468c496f43d01a5f9cdb
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-13 10:10:28 +0600

    Multiple choice task screen rudimentary work done

--- App/earn_easy/lib/app_screens/task/multiple_option_task.dart
+++ App/earn_easy/lib/app_screens/task/multiple_option_task.dart
@@ -13,7 +13,7 @@ class MultipleChoiceTaskScreen extends StatefulWidget {
 
 class _MultipleChoiceTaskScreenState extends State<MultipleChoiceTaskScreen> {
   MultipleChoiceTask _multipleChoiceTask;
-
+  int _groupValue ;
   @override
   Widget build(BuildContext context) {
     _multipleChoiceTask = Provider.of<MultipleChoiceTask>(context);
@@ -45,6 +45,55 @@ class _MultipleChoiceTaskScreenState extends State<MultipleChoiceTaskScreen> {
                       "${_multipleChoiceTask.taskDescription}",
                       textScaleFactor: 1.5,
                       style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ListView.builder(
+                      itemCount: _multipleChoiceTask.optionList.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return RadioListTile(
+                          title: Text(
+                            _multipleChoiceTask.optionList[index].option,
+                            textScaleFactor: 1.2,
+                          ),
+                          controlAffinity: ListTileControlAffinity.leading,
+                          activeColor: Colors.deepPurple,
+                          selected: _groupValue == index,
+                          value:index,
+                          groupValue: _groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              _groupValue = value;
+                              print("Value $_groupValue");
+                              _multipleChoiceTask.optionList[index].checked = _groupValue == index;
+                            });
+                          },
+                        );
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: RaisedButton.icon(
+                        elevation: 5.0,
+                        color: Colors.white,
+                        label: Text(
+                          "Finish Task",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        icon: Icon(
+                          Icons.cloud_upload,
+                          color: Colors.blueAccent,
+                        ),
+                        onPressed: () async {
+                          //compressImageFromImageFile();
+                          //await uploadToFirebase();
+                        },
+                      ),
                     ),
                   ],
                 ),
