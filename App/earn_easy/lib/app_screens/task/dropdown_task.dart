a3de9dbadb85147ed331f3e1cb8ac9afc67b438a
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-13 08:31:07 +0600

    dropdown_task.dart DropdownTaskScreen class initialized

---
+++ App/earn_easy/lib/app_screens/task/dropdown_task.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
+
+class DropdownTaskScreen extends StatefulWidget {
+  @override
+  _DropdownTaskScreenState createState() => _DropdownTaskScreenState();
+}
+
+class _DropdownTaskScreenState extends State<DropdownTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Dropdown Task"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.chevronCircleDown,
+                  size: 20.0,
+                ),
+                SizedBox(width: 10.0),
+                Text("Dropdown"),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              //"${_imageTask.taskDescription}",
+              "Header",
+              textScaleFactor: 1.5,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
