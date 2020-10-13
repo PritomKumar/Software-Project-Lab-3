9d3d0af7dbf01953f387e3624f1b50b98e6b55a5
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-13 08:26:05 +0600

    checkbox_task.dart CheckboxTaskScreen initialized

---
+++ App/earn_easy/lib/app_screens/task/checkbox_task.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
+
+class CheckBoxTaskScreen extends StatefulWidget {
+  @override
+  _CheckBoxTaskScreenState createState() => _CheckBoxTaskScreenState();
+}
+
+class _CheckBoxTaskScreenState extends State<CheckBoxTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Checkbox Task"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.checkSquare,
+                  size: 20.0,
+                ),
+                SizedBox(width: 10.0),
+                Text("Checkbox"),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              //"${_imageTask.taskDescription}",
+              "Header",
+              textScaleFactor: 1.5,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
