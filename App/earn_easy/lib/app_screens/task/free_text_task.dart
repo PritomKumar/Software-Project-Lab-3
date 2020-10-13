529a661e602161d689e88a16a3584c615c810274
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-13 08:32:57 +0600

    free_text_task.dart FreeTextTaskScreen class initialized

---
+++ App/earn_easy/lib/app_screens/task/free_text_task.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
+
+class FreeTextTaskScreen extends StatefulWidget {
+  @override
+  _FreeTextTaskScreenState createState() => _FreeTextTaskScreenState();
+}
+
+class _FreeTextTaskScreenState extends State<FreeTextTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Free Text Task"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.alignLeft,
+                  size: 20.0,
+                ),
+                SizedBox(width: 10.0),
+                Text("Free Text"),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              //"${_imageTask.taskDescription}",
+              "Header",
+              textScaleFactor: 1.5,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
