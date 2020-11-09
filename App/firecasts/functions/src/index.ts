69806ed21b772269d174d2e1112d8afc9a85e273
PritomKumar <bsse0919@iit.du.ac.bd>
2020-11-09 16:43:59 +0600

    Checking console log!

--- App/firecasts/functions/src/index.ts
+++ App/firecasts/functions/src/index.ts
@@ -5,5 +5,6 @@ import * as functions from 'firebase-functions';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  console.log("Console log hello");
+  response.send("Hello from Firebase HA ha !");
 });
