5e1c3a103f9ef57a0e15ef6cf78f125ad1dffa96
PritomKumar <bsse0919@iit.du.ac.bd>
2020-11-10 10:43:38 +0600

    new work

--- App/firecasts/functions/src/index.ts
+++ App/firecasts/functions/src/index.ts
@@ -1,10 +1,27 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  console.log("Console log hello");
-  response.send("Hello from Firebase HA ha !");
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   console.log("Console log hello");
+//   response.send("Hello from Firebase HA ha !");
+// });
+
+export const getSingleGigInfo = functions.https.onRequest((request, response) => {
+  admin.firestore().doc('Gigs/A1vk0mCKxrIVPPmuyfEs').get()
+  .then(snapshot=>{
+    const data = snapshot.data()
+    response.send(data)
+  })
+  .catch(error=>{
+    console.log("Error")
+    response.status(500).send(error)
+  })
 });
