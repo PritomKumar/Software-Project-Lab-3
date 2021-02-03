33f08070cebd5164952aedd6fb909508abeaa0f8
PritomKumar <bsse0919@iit.du.ac.bd>
2021-02-03 20:14:47 +0600

    trying themedata changer with flutter

---
+++ App/earn_easy/lib/shared/ThemeChanger.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeChanger with ChangeNotifier {
+  ThemeData _themeData;
+
+  ThemeChanger(this._themeData);
+
+  getTheme() => _themeData;
+
+  setTheme(ThemeData theme) {
+    _themeData = theme;
+  }
+
+  @override
+  void notifyListeners() {
+    // TODO: implement notifyListeners
+    super.notifyListeners();
+  }
+}
