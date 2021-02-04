8c91e5e90ce3d33c3b2d46e2e3e06f420da6a535
PritomKumar <bsse0919@iit.du.ac.bd>
2021-02-04 19:49:46 +0600

    Shared prefs not working

--- App/earn_easy/lib/app_screens/theme/theme_changer.dart
+++ App/earn_easy/lib/app_screens/theme/theme_changer.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ThemeChanger with ChangeNotifier {
   ThemeData _themeData;
   SharedPreferences _sharedPreferences;
-  String _theme_data_color_key = "theme_data_color";
+  String key = "theme_data_color";
   String _theme_data_text_key = "theme_data_text";
 
   ThemeChanger(ThemeData _themeData) {
@@ -28,18 +28,59 @@ class ThemeChanger with ChangeNotifier {
 
   _loadFromPrefs() async {
     await _initPrefs();
-    ThemeData.from(colorScheme: ColorScheme(primary: Colors.blue));
-    _themeData = ThemeData.from(
-            colorScheme: ColorScheme(
-                primary:
-                    Color(_sharedPreferences.getInt(_theme_data_color_key)))) ??
+    _themeData = _sharedPreferences.getString(key) ??
         ThemeData(primarySwatch: Colors.blue);
+    debugPrint("shared preference = ${_sharedPreferences.getString(key)}");
     notifyListeners();
   }
 
   _saveToPrefs() async {
     await _initPrefs();
-    _sharedPreferences.setInt(
-        _theme_data_color_key, _themeData.primaryColor.value);
+    _sharedPreferences.setString(key, _themeData.toString());
+    debugPrint("shared preference = ${_sharedPreferences.getString(key)}");
   }
 }
+
+//
+// class ThemeChanger with ChangeNotifier {
+//   ThemeData _themeData;
+//   SharedPreferences _sharedPreferences;
+//   String _theme_data_color_key = "theme_data_color";
+//   String _theme_data_text_key = "theme_data_text";
+//
+//   ThemeChanger(ThemeData _themeData) {
+//     this._themeData = _themeData;
+//     _loadFromPrefs();
+//   }
+//
+//   getTheme() => _themeData;
+//
+//   setTheme(ThemeData theme) {
+//     _themeData = theme;
+//     _saveToPrefs();
+//     notifyListeners();
+//   }
+//
+//   _initPrefs() async {
+//     if (_sharedPreferences == null) {
+//       _sharedPreferences = await SharedPreferences.getInstance();
+//     }
+//   }
+//
+//   _loadFromPrefs() async {
+//     await _initPrefs();
+//     ThemeData.from(colorScheme: ColorScheme(primary: Colors.blue));
+//     _themeData = ThemeData.from(
+//         colorScheme: ColorScheme(
+//             primary:
+//             Color(_sharedPreferences.getInt(_theme_data_color_key)))) ??
+//         ThemeData(primarySwatch: Colors.blue);
+//     notifyListeners();
+//   }
+//
+//   _saveToPrefs() async {
+//     await _initPrefs();
+//     _sharedPreferences.setInt(
+//         _theme_data_color_key, _themeData.primaryColor.value);
+//   }
+// }
