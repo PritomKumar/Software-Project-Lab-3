83c6500aaee84a50730ffe088e0dc53e782f8222
PritomKumar <bsse0919@iit.du.ac.bd>
2021-03-28 23:14:51 +0600

    getAll attempted users is working for https req

--- App/firecasts/functions/src/index.ts
+++ App/firecasts/functions/src/index.ts
@@ -1,32 +1,74 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 admin.initializeApp();
-
-
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello Demon lord!", {structuredData: true});
-  console.log("Console log hello");
-  response.send("Hello from Pain!");
+    functions.logger.info("Hello Demon lord!", { structuredData: true });
+    console.log("Console log hello");
+    response.send("Hello from Pain!");
 });
 
-export const getGigInfo = functions.https.onRequest(async (request, response) => {
-  try {
-    const snapshotss = await admin.firestore().doc('Gigs/UpTzbCY8JsWBuTHqWl8K/Tasks/FHia6YicE6m9RCag0wr6/').get();
-    const data = snapshotss.data();
-    console.log("Data loading !!");
-    console.log(data);
-    response.send(data);
-  } catch (error) {
-    console.log("Error")
-    response.status(500).send(error)
-  }
-  
+export const getGigInfo = functions.https.onRequest(
+    async (request, response) => {
+        try {
+            const snapshotss = await admin
+                .firestore()
+                .doc("Gigs/UpTzbCY8JsWBuTHqWl8K/Tasks/FHia6YicE6m9RCag0wr6/")
+                .get();
+            const data = snapshotss.data();
+            console.log("Data loading !!");
+            console.log(data);
+            response.send(data);
+        } catch (error) {
+            console.log("Error");
+            response.status(500).send(error);
+        }
+    }
+);
 
-});
+export const getAllAttemptedUsers = functions.https.onRequest(
+    async (request, response) => {
+        try {
+            const snapshotss = await admin
+                .firestore()
+                .doc("Gigs/UpTzbCY8JsWBuTHqWl8K/")
+                .get();
 
+            const attempData = snapshotss.data();
+            if (attempData) {
+                const attempUsers = attempData.attemptedUsers;
 
+                const promises: any[] = [];
+                attempUsers.forEach((user:any) => {
+                    const userInUsersDocument = admin
+                        .firestore()
+                        .doc(`Users/${user}`)
+                        .get();
+                    promises.push(userInUsersDocument);
+                });
+
+                const reply = await Promise.all(promises);
+
+                const results: any[] = [];
+                if (reply) {
+                    reply.forEach((snap) => {
+                        const data = snap.data();
+                        results.push(data);
+                    });
+                }
+                console.log(results);
+                response.send(results);
+            } else {
+                console.log("Error");
+                response.status(500).send("No Data");
+            }
+        } catch (error) {
+            console.log("Error");
+            response.status(500).send(error);
+        }
+    }
+);
