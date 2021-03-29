0439258de43ccbe887032a60691ed14054e1cd13
PritomKumar <bsse0919@iit.du.ac.bd>
2021-03-29 14:11:35 +0600

    calcuateScoresBasedOnDistance done

--- App/firecasts/functions/src/index.ts
+++ App/firecasts/functions/src/index.ts
@@ -30,6 +30,15 @@ admin.initializeApp();
 //     }
 // );
 
+//distance = 500
+//level = 200
+//rating = 100
+//misconduct = 0 to negative
+//marital status = 50
+//employmentStatus = 50 ? push notification send
+//household income = 50
+//education = 50
+// total = 1000
 export const getAllAttemptedUsers = functions.https.onRequest(
     async (request, response) => {
         try {
@@ -41,14 +50,24 @@ export const getAllAttemptedUsers = functions.https.onRequest(
             const attempData = snapshotss.data();
             if (attempData) {
                 const attempUsers = attempData.attemptedUsers;
-
-                const promises: any[] = [];
+                const filteredUsers: any[] = [];
                 attempUsers.forEach((singleUser: any) => {
+                    if (firstFilteringBasedOnBasicInfo(singleUser)) {
+                        filteredUsers.push(singleUser);
+                    }
+                });
+                var scores: number[] = [];
+                const promises: any[] = [];
+                var i = 0;
+                filteredUsers.forEach((singleUser: any) => {
+                    scores[i] = calcuateScoresBasedOnDistance(singleUser);
                     const userInUsersDocument = admin
                         .firestore()
                         .doc(`Users/${singleUser.uid}`)
                         .get();
+
                     promises.push(userInUsersDocument);
+                    i++;
                 });
 
                 const reply = await Promise.all(promises);
@@ -76,4 +95,29 @@ export const getAllAttemptedUsers = functions.https.onRequest(
     }
 );
 
-void async function calculateHeuristic(user: any) {};
+function firstFilteringBasedOnBasicInfo(user: any): boolean {
+    if (user.distance < 15.0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function calcuateScoresBasedOnDistance(singleUser: any): number {
+    if (singleUser.distance >= 0.0 && singleUser.distance <= 1.0) {
+        return 500;
+    } else if (singleUser.distance > 1.0 && singleUser.distance <= 2.0) {
+        return 400 + 100 * (1 - (singleUser.distance - 1.0 / 1.0));
+    } else if (singleUser.distance > 2.0 && singleUser.distance <= 3.5) {
+        return 300 + 100 * (1 - (singleUser.distance - 2.0) / 1.5);
+    } else if (singleUser.distance > 3.5 && singleUser.distance <= 5.0) {
+        return 200 + 100 * (1 - (singleUser.distance - 3.5) / 1.5);
+    } else if (singleUser.distance > 5.0 && singleUser.distance <= 8.0) {
+        return 100 + 100 * (1 - (singleUser.distance - 5.0) / 3.0);
+    } else if (singleUser.distance > 8.0 && singleUser.distance <= 11.0) {
+        return 50 + 50 * (1 - (singleUser.distance - 8.0) / 3.0);
+    } else if (singleUser.distance > 11.0 && singleUser.distance <= 15.0) {
+        return 0 + 50 * (1 - (singleUser.distance - 11.0) / 4.0);
+    } else {
+        return 0.0;
+    }
+}
