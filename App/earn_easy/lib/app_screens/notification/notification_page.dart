c8d5beb61875e03b5fd21d670efc5782e99910db
PritomKumar <bsse0919@iit.du.ac.bd>
2021-04-02 16:16:58 +0600

    push notification working but problem with onLaunch

--- App/earn_easy/lib/app_screens/notification/notification_page.dart
+++ App/earn_easy/lib/app_screens/notification/notification_page.dart
@@ -3,35 +3,12 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: Theme.of(context),
-      home: SafeArea(
-        child: Scaffold(
-          drawer: SideDrawer(),
-          appBar: AppBar(
-            title: Text('Notification'),
-          ),
-          body: Center(
-            child: Container(
-              child: Text('Notification'),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  _NotificationPageState createState() => _NotificationPageState();
 }
 
-class FirebaseMessagingDemo extends StatefulWidget {
-  @override
-  _FirebaseMessagingDemoState createState() => _FirebaseMessagingDemoState();
-}
-
-class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
+class _NotificationPageState extends State<NotificationPage> {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
   List<Message> _messagesList = <Message>[];
 
@@ -41,60 +18,96 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
     });
   }
 
+  _getPermission() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
   _configureFirebaseListeners() {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       RemoteNotification notification = message.notification;
       AndroidNotification android = message.notification?.android;
-      _setMessage(message.data);
+      _setMessage(message);
       print(
           "On Message notification = ${notification.title}  and android = ${android.channelId}");
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      _setMessage(message.data);
+      _setMessage(message);
       print('A new onMessageOpenedApp event was published! ${message.data}');
+    });
+    FirebaseMessaging.onBackgroundMessage((RemoteMessage message) {
+      _setMessage(message);
+      print(' onBackgroundMessage ${message.data}');
+      return;
     });
   }
 
-  _setMessage(Map<String, dynamic> message) {
-    final notification = message['notification'];
-    final data = message['data'];
-    final String title = notification['title'];
-    final String body = notification['body'];
-    final String mMessage = data['message'];
-    Message m = Message(title: title, body: body, message: mMessage);
-    _messagesList.add(m);
+  _setMessage(RemoteMessage message) {
+    final notification = message.notification;
+    final data = message.data;
+    final String title = notification.title;
+    final String body = notification.body;
+    final String mMessage = data.toString();
+    setState(() {
+      Message m = Message(title: title, body: body, message: mMessage);
+      _messagesList.add(m);
+    });
   }
 
   @override
   void initState() {
     super.initState();
+    _getPermission();
     _getToken();
     _configureFirebaseListeners();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Demo"),
-      ),
-      body: ListView.builder(
-        itemCount: _messagesList == null ? 0 : _messagesList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Text(
-                _messagesList[index].message,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.black,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context),
+      home: SafeArea(
+        child: Scaffold(
+          drawer: SideDrawer(),
+          appBar: AppBar(
+            title: Text("Demo"),
+          ),
+          body: ListView.builder(
+            itemCount: _messagesList == null ? 0 : _messagesList.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(
+                    _messagesList[index].body,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          );
-        },
+              );
+            },
+          ),
+        ),
       ),
     );
   }
